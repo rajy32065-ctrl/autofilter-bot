594aa09d05f7e53cf590ad5b453806b7c1915ca9
rajy32065-ctrl <rajy32065@gmail.com>
2026-01-04 17:55:09 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+MONGO_URI = "mongodb+srv://user:pass@cluster.mongodb.net/autofilter"
+
+MOVIE_CHANNEL = -1001234567890
+FORCE_SUB_CHANNEL = -1009876543210
+
+ADMINS = [123456789]
+RESULTS_PER_PAGE = 5
